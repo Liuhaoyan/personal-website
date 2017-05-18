90a5fede8e98e8a4d77535f22caa24118e6bbd4d
Liuhaoyan <854215903@qq.com>
2017-05-18 21:03:47 +0800

    js文件上传

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+
+/*requirejs*/
+require(['a'], function(sortArr){
+	// var arr = [5, 3, 4, 9, 33];
+	adsads
+	adsadsasdas
+
+	adsads
+	sssad
+	var arr = 'sadf';
+	console.log(sortArr(arr));
+});
+
+/*seajs*/
+define(function(require){
+	adsadsasdas
+	adsadsasdas
+	defisdf
+	var arr = [5, 3, 4, 9, 33];
+	var sortArr = require('a');
+	console.log(sortArr(arr));
+
+
+});
+
+/*
+AMD(requirejs):异步模块定义:Asynchronization Module Definition
+CMD(seajs):通用模块定义:Common Module Definition
+AMD 推崇依赖前置
+CMD 推崇依赖就近
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
